c1e7bca8fc8af2c582ff1918910eb30096440f1c
jennlew <nlewedum@live.com>
2021-11-02 19:53:52 +0000

    queue and stack methods added

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -28,3 +28,26 @@ class BinarySearchTree:
             self.root = Node(data)
         else:
             self._insert_recursive(data, self.root)
+
+    def _search_recursive(self, blog_post_id, node):
+        if blog_post_id == node.data["id"]:
+            return node.data
+
+        if blog_post_id < node.data["id"] and node.left is not None:
+            if blog_post_id == node.left.data["id"]:
+                return node.left.data
+            return self._search_recursive(blog_post_id, node.left)
+
+        if blog_post_id > node.data["id"] and node.right is not None:
+            if blog_post_id == node.right.data["id"]:
+                return node.right.data
+            return self._search_recursive(blog_post_id, node.right)
+
+        return False
+
+    def search(self, blog_post_id):
+        blog_post_id = int(blog_post_id)
+        if self.root is None:
+            return False
+
+        return self._search_recursive(blog_post_id, self.root)
